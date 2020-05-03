e5e419705be3e53089415bc7dbd43007970bcca7
mpeng <support@theoptionslab.com>
2020-05-03 12:30:02 -0400

    Add some Java test

---
+++ Duckie.java
@@ -0,0 +1,10 @@
+public class Duckie {
+
+	public static void main( String argv[] ) {
+	
+		for ( int i = 0; i < 10; i++ )
+			System.out.println( "Hello World !" + argv[0] );
+
+	}
+
+}
---
+++ python.py
@@ -0,0 +1,1 @@
+print "My Duckie can do calculation" 
